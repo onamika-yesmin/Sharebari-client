713694cc7d36b2b9d33efb638017f783898ba7e4
onamika yesmin <onamikaonu086@gmail.com>
2026-07-12 22:40:44 +0600

    chore(client): finalize lint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,12 @@
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTypeScript from "eslint-config-next/typescript";
 
-const eslintConfig = [...nextVitals, ...nextTypeScript];
+const eslintConfig = [
+  {
+    ignores: [".next/**", ".next-sharebari/**", "out/**", "build/**", "next-env.d.ts"],
+  },
+  ...nextVitals,
+  ...nextTypeScript,
+];
 
 export default eslintConfig;
